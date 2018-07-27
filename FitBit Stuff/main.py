2d5baafabebc1f60a2f9e39c0fc75777e7153289
Demo User <demouser@MacBook-Pro.local>
2018-07-27 08:29:25 -0700

    Gadget Project

---
+++ FitBit Stuff/main.py
@@ -0,0 +1,14 @@
+import webapp2
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Welcome to Brooke's Online Portal")
+
+
+
+app = webapp2.WSGIApplication([
+    ('/fitbit', MainHandler),
+
+
+], debug=True)
